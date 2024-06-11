2f74575db60c840de51c41751d023d476261aa50
Latreth <florentguepin5@gmail.com>
2024-06-11 14:49:20 +0200

    Update run_experiment_synthetic_only.sh

--- scripts/run_experiment_synthetic_only.sh
+++ scripts/run_experiment_synthetic_only.sh
@@ -1,10 +1,5 @@
-#PBS -l walltime=01:00:00
-#PBS -l select=1:ncpus=20:mem=200gb
+#!/bin/bash
 
-module load anaconda3/personal
-source activate featuresnout
-
-cd /rds/general/user/frg20/home/featuresnout/Git/featuresnout/
 python synthetic_only_main.py --path_to_data='./data/2011 Census Microdata Teaching File_OG.csv' \
                 --path_to_metadata='./data/2011 Census Microdata Teaching Discretized.json' \
                     --target_record_id=266579\
